60e09dc3cc5133f0be8df6f0b5c74ec49d8262db
Americo <americo@mercury.com>
2022-02-11 15:50:41 -0700

    Add better checks to ch11e

--- src/chapter11/ch11eHuttonsRazor.hs
+++ src/chapter11/ch11eHuttonsRazor.hs
@@ -7,11 +7,11 @@ eval (Lit x  ) = x
 eval (Add x y) = eval x + eval y
 
 evalTest :: Integer
-evalTest = eval (Add (Lit 1) (Lit 9001))
+evalTest = eval (Add (Add (Lit 1) (Lit 3)) (Lit 9001))
 
 printExpr :: Expr -> String
 printExpr (Lit x  ) = show x
 printExpr (Add x y) = printExpr x <> " + " <> printExpr y
 
 printExprTest :: String
-printExprTest = printExpr (Add (Lit 1) (Lit 9001))
+printExprTest = printExpr (Add (Add (Lit 1) (Lit 3)) (Lit 9001))
