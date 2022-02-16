d4c7a4eff47b964b2715bc984a535057555c857b
Americo <americo@mercury.com>
2022-02-16 14:33:01 -0700

    Get hangman project started in parent project, tune stack/cabal setup

--- shell.nix
+++ shell.nix
@@ -1,6 +1,27 @@
-with import <nixpkgs> {};
-mkShell {
+# From https://gist.github.com/Gabriel439/4c0c2a81cc0e089043ed33da5b01fee7
+
+let
+  nixpkgs = builtins.fetchTarball {
+    url =
+      "https://github.com/NixOS/nixpkgs/archive/7e003d7fb9eff8ecb84405360c75c716cdd1f79f.tar.gz";
+    sha256 = "08y8pmz7xa58mrk52nafgnnrryxsmya9qaab3nccg18jifs5gyal";
+  };
+
+  config.allowBroken = true;
+
+  pkgs = import nixpkgs { inherit config; };
+
+in pkgs.mkShell {
   nativeBuildInputs = [
-    stack 
+    pkgs.automake
+    pkgs.autoconf
+    pkgs.python3
+    (pkgs.haskell.packages.ghc902.ghcWithPackages
+      (p: [ p.alex p.happy p.haddock ]))
+    pkgs.sphinx
+    pkgs.texlive.combined.scheme-small
+    pkgs.gmp
+    pkgs.stack
+    pkgs.nixfmt
   ];
 }
---
+++ src/chapter13/hangman/src/Main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import           Control.Monad                  ( forever )
+import           Data.Char                      ( toLower )
+import           Data.List                      ( intersperse )
+import           Data.Maybe                     ( isJust )
+import           System.Exit                    ( exitSuccess )
+import           System.IO                      ( BufferMode(NoBuffering)
+                                                , hSetBuffering
+                                                , stdout
+                                                )
+import           System.Random                  ( randomRIO )
+
+main :: IO ()
+main = do
+  putStrLn "Hangman"
